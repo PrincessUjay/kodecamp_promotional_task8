fa9bf7fc85f436ea3aed93a8e76dafc79182b721
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-08 06:36:05 +0100

    Update main.tf

---
+++ terraform/modules/ec2_instance/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_key_pair" "key_pair" {
+  key_name           = "KCVPCkeypair"
+  include_public_key = true
+}
+
+resource "aws_instance" "public_instance" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
+  security_groups         = [var.public_sg_id]
+  associate_public_ip_address = true
+  key_name                    = data.aws_key_pair.key_pair.key_name
+
+  user_data = file("${path.module}/scripts/install_nginx.sh")
+
+  tags = {
+    Name = "PublicInstance"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami               = var.ami
+  instance_type     = var.instance_type
+  subnet_id         = var.private_subnet_id
+  security_groups    = [var.private_sg_id]
+  key_name                    = data.aws_key_pair.key_pair.key_name
+
+  user_data = file("${path.module}/scripts/install_postgresql.sh")
+
+  tags = {
+    Name = "PrivateInstance"
+  }
+}
+
+resource "aws_instance" "minikube" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.minikube_subnet_id
+  key_name      = data.aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "MinikubeInstance"
+ }
+
+ provisioner "file" {
+ source      = "${path.module}/scripts/install_minikube.sh"
+ destination = "/tmp/install_minikube.sh"
+ }
+
+ provisioner "remote-exec" {
+   inline = [
+  "chmod +x /tmp/install_minikube.sh",
+  "/tmp/install_minikube.sh"
+   ]
+ }
+
+ connection {
+   type        = "ssh"
+   user        = "ubuntu"
+   private_key = file(var.ssh_key_path)
+   host        = self.public_ip
+ }
+}    
