8a54b2787159919f1f928147a8d98369f0eca16b
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-08 06:40:23 +0100

    Update variables.tf

---
+++ terraform/modules/ec2_instance/variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  description = "The AWS region to create resources in."
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID to use for the instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to launch"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "ID of the public subnet"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "ID of the private subnet"
+  type        = string
+}
+
+variable "public_sg_id" {
+  description = "ID of the public security group"
+  type        = string
+}
+
+variable "private_sg_id" {
+  description = "ID of the private security group"
+  type        = string
+}
+
+variable "minikube_subnet_id" {
+  description = "The ID of the subnet for the Minikube instance."
+  type        = string
+}
+
+variable "ssh_key_path" {
+  description = "The path to the SSH key to use for connecting to the instance."
+  type        = string
+}
