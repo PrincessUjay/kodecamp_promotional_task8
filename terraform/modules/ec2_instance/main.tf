56871bfb6b08f9f73d9feb07d7048c476305d28f
PrincessUjay <onyekabaprincess@gmail.com>
2024-08-09 17:25:28 -0700

    commit 1

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -39,29 +39,9 @@ resource "aws_instance" "minikube" {
    security_groups = [var.minikube_sg_id]
    key_name      = data.aws_key_pair.key_pair.key_name
 
+   user_data = file("${path.module}/scripts/install_minikube.sh")
+
    tags = {
      Name = "MinikubeInstance"
    }
-
-  provisioner "file" {
-  source      = "${path.module}/scripts/install_minikube.sh"
-  destination = "/tmp/install_minikube.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-  "chmod +x /tmp/install_minikube.sh",
-  "/tmp/install_minikube.sh"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("C:/Users/HP/.ssh/KCVPCkeypair1.pem")
-    host        = self.public_ip
-    timeout     = "10m"
-  }
-
-  depends_on = [aws_instance.minikube]
-}    
+}
--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -62,12 +62,13 @@ resource "aws_security_group" "minikube_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+ ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
