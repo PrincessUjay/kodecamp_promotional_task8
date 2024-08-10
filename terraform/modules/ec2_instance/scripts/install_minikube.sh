663008ba548022befa85a0031b8d4c251b19a8b3
PrincessUjay <onyekabaprincess@gmail.com>
2024-08-09 19:09:45 -0700

    task8hardo

--- terraform/modules/ec2_instance/scripts/install_minikube.sh
+++ terraform/modules/ec2_instance/scripts/install_minikube.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start --driver=docker
+minikube start --driver=docker --memory=4096 --cpus=2
 kubectl apply -f /k8s/deployment.yaml
 kubectl apply -f /k8s/service.yaml
