c92a0fe783012637d94e7b1f9a41157d5c26b20a
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-14 18:58:32 +0100

    Update install_minikube.sh

--- terraform/modules/ec2_instance/scripts/install_minikube.sh
+++ terraform/modules/ec2_instance/scripts/install_minikube.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-set -x
-
 sudo apt-get update
 sudo apt-get install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker $USER
-sudo apt-get install -y conntrack
-
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-curl -LO minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-sudo mv minikube /usr/local/bin/
-
-# start minikube and apply the kubernetes manifests
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=docker --memory=4096 --cpus=2
+kubectl apply -f /kodecamp_promotional_task8/k8s/deployment.yaml
+kubectl apply -f /kodecamp_promotional_task8/k8s/service.yaml
