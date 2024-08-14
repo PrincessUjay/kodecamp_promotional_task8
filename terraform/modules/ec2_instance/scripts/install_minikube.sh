0cc4b723f2fb6b07c245de2e389842798b6187db
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-14 15:55:40 +0100

    Update install_minikube.sh

--- terraform/modules/ec2_instance/scripts/install_minikube.sh
+++ terraform/modules/ec2_instance/scripts/install_minikube.sh
@@ -1,54 +1,18 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y docker.io
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+sudo apt-get install -y conntrack
 
-# create kubernetes deployment.yaml
-cat <<EOF > /home/ubuntu/k8s/deployment.yaml
-apiVersion: apps/v2
-kind: Deployment
-metadata:
-  name: myfirstpythonapp
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: myfirstpythonapp
-  template:
-    metadata:
-      labels:
-        app: myfirstpythonapp
-    spec:
-      containers:
-      - name: myfirstpythonapp
-        image: princessujay/myfirstpythonapp:0.0.2.RELEASE
-        ports:
-        - containerPort: 8000
-        resources:
-          limits:
-            memory: "512Mi"
-            cpu: "1000m"
-          requests:
-            memory: "256Mi"
-            cpu: "500m"
-EOF
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
-#create kubernetes service.yaml
-cat <<EOF > /home/ubuntu/k8s/service.yaml
-apiVersion: v2
-kind: Service
-metadata:
-  name: myfirstpythonapp-service
-spec:
-  type: ClusterIP
-  selector:
-     app: myfirstpythonapp
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 8000
-EOF
+curl -LO minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
 
 # start minikube and apply the kubernetes manifests
 minikube start --driver=docker --memory=4096 --cpus=2
