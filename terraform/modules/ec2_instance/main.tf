897601bd2bf585b5862de83c00ed4b63cd88a3f7
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-14 09:00:08 +0000

    Update main.tf

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -45,23 +45,37 @@ resource "aws_instance" "minikube" {
      Name = "MinikubeInstance"
    }
 
+   # First, provision the Minikube installation script
    provisioner "file" {
-    source = "C:/Users/HP/kodecamp_promotional_task8/k8s"
-    destination = "/home/ubuntu/k8s"
+     source      = "${path.module}/scripts/install_minikube.sh"
+     destination = "/tmp/install_minikube.sh"
    }
 
    provisioner "remote-exec" {
-    inline = [
-      "mkdir -p /home/ubuntu/k8s",
-      "sudo chown -R ubuntu:ubuntu /home/ubuntu/k8s",
-      "ls -al /home/ubuntu/k8s"
-    ]
+     inline = [
+       "chmod +x /tmp/install_minikube.sh",
+       "/tmp/install_minikube.sh"
+     ]
+   }
+
+   # Then, provision the Kubernetes manifest files
+   provisioner "file" {
+     source = "C:/Users/HP/kodecamp_promotional_task8/k8s"
+     destination = "/home/ubuntu/k8s"
+   }
+
+   provisioner "remote-exec" {
+     inline = [
+       "mkdir -p /home/ubuntu/k8s",
+       "sudo chown -R ubuntu:ubuntu /home/ubuntu/k8s",
+       "ls -al /home/ubuntu/k8s"
+     ]
    }
 
    connection {
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file("C:/Users/HP/.ssh/KCVPCkeypair1.pem")
-    host = self.public_ip
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("C:/Users/HP/.ssh/KCVPCkeypair1.pem")
+     host = self.public_ip
    }
 }
