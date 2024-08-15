f408d8528ab246a0e0396c10d701158bee9c39d4
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-15 13:46:23 +0100

    Update install_minikube.sh

--- terraform/modules/ec2_instance/scripts/install_minikube.sh
+++ terraform/modules/ec2_instance/scripts/install_minikube.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-sudo apt-get update
+# Update the system
+sudo apt-get update -y
+
+# Install Docker
 sudo apt-get install -y docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker $USER
+
+# Install conntrack package (required by Minikube)
 sudo apt-get install -y conntrack
 
+# Install Kubectl
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-curl -LO minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# Install Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
-sudo mv minikube /usr/local/bin/
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-
-# start minikube and apply the kubernetes manifests
+sudo mv minikube /usr/local/bin/`
