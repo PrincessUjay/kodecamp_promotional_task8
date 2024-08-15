3a79c66c5b32225325dc166120bf53236bc881a1
PrincessUjay <onyekabaprincess@gmail.com>
2024-08-15 05:24:39 -0700

    commit 1

--- terraform/modules/ec2_instance/scripts/install_minikube.sh
+++ terraform/modules/ec2_instance/scripts/install_minikube.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 sudo usermod -aG docker $USER
 sudo apt-get install -y conntrack
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
@@ -12,5 +11,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl -LO minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 # start minikube and apply the kubernetes manifests
